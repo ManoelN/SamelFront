8394b179c5d03f07dd209f33176f401664612c4c
ManoelN <manoelferreneto@gmail.com>
2021-04-28 20:17:23 -0400

    segunda versão script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let time = 5000,
     currentImageIndex = 0,
     images = document
-                .querySelectorAll("#slider img")
+                .querySelectorAll("#carousel slide img")
     max = images.length;
 
 function nextImage() {
 
     images[currentImageIndex]
-        .classList.remove("selected")
+        .classList.remove("img")
 
     currentImageIndex++
 
@@ -15,7 +15,7 @@ function nextImage() {
         currentImageIndex = 0
 
     images[currentImageIndex]
-        .classList.add("selected")
+        .classList.add("img")
 }
 
 function start() {
@@ -26,3 +26,19 @@ function start() {
 }
 
 window.addEventListener("load", start)
+
+
+function sendRequest(file, key) {
+    var market = document.getElementById('mkt').value;
+    var safeSearch = document.getElementById('safesearch').value;
+    var baseUri = 'https://api-teste.samel.com.br/fotos`;
+
+    var form = new FormData();
+    form.append("img", file);
+
+    var request = new XMLHttpRequest();
+
+    request.open("POST", baseUri);
+    request.addEventListener('load', handleResponse);
+    request.send(form);
+}
